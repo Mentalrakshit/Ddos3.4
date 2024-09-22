b05c83002b7c012a18a65e401ce8eb910e8a62c7
Mentalrakshit <rakshitmohan18@gmail.com>
2024-09-22 04:25:55 +0000

    Pending changes exported from your codespace

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/sahil/sahil.py",
+    "postStartCommand": "python3 /workspaces/Ddos3.4/sahil.py",
     "customizations": {
         "vscode": {
             "settings": {
